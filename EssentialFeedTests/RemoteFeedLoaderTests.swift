3525ea971481a83e20f60eafcceb563733fbe42f
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-01-23 23:14:49 +0100

    Initialize `RemoteFeedLoader` with a `URL` object.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,13 +10,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load() {
-        self.client.get(from: URL(string: "https://a-url.com")!)
+        self.client.get(from: self.url)
     }
 }
 
@@ -35,18 +37,20 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://a-url.com")!
         let client = HTTPClientSpy.init()
-        let _ = RemoteFeedLoader.init(client: client)
+        let _ = RemoteFeedLoader.init(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy.init()
-        let sut = RemoteFeedLoader.init(client: client)
+        let sut = RemoteFeedLoader.init(url: url, client: client)
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 
 }
