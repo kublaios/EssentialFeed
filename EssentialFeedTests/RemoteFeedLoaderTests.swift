2c02cf4759cf74a9fe75edffe097a496074fa7ab
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-01-23 23:19:30 +0100

    Refactor `RemoteFeedLoaderTests` to comply with DRY principle.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -26,31 +26,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-
-    func get(from url: URL) {
-        self.requestedURL = url
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy.init()
-        let _ = RemoteFeedLoader.init(url: url, client: client)
+        let (_, client) = self.makeSUT()
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy.init()
-        let sut = RemoteFeedLoader.init(url: url, client: client)
+        let (sut, client) = self.makeSUT(url: url)
         sut.load()
 
         XCTAssertEqual(client.requestedURL, url)
+    }
+
+    // MARK: Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!)
+    -> (sut: RemoteFeedLoader, client: HTTPClientSpy)
+    {
+        let client = HTTPClientSpy.init()
+        return (RemoteFeedLoader.init(url: url, client: client), client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+
+        func get(from url: URL) {
+            self.requestedURL = url
+        }
     }
 
 }
