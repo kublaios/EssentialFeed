0951f136f06763a433ebe23954d679a0902d601c
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-01-24 15:44:13 +0100

    Enable loading URL multiple times.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -24,6 +24,15 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
 
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = self.makeSUT(url: url)
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
     // MARK: Helpers
 
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!)
@@ -35,9 +44,11 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs: [URL] = []
 
         func get(from url: URL) {
             self.requestedURL = url
+            self.requestedURLs.append(url)
         }
     }
 
