2c4d672ba566bfae188bb391b2cf311655a1bf6f
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-01-29 23:48:47 +0100

    Test `get(from:)` returns data task only once.

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -17,7 +17,7 @@ class URLSessionHTTPClient {
     }
 
     func get(from url: URL) {
-        self.session.dataTask(with: url)
+        self.session.dataTask(with: url).resume()
     }
 }
 // ---
@@ -34,17 +34,46 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
 
+    func test_getFromURL_resumesDataTaskOnCreationOnlyOnce() {
+        let url = URL(string: "https://a-url.com")!
+        let session = URLSessionSpy.init()
+        let task = URLSessionDataTaskSpy.init()
+        let sut = URLSessionHTTPClient.init(session: session)
+
+        session.stub(url: url, task: task)
+        sut.get(from: url)
+
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+
 }
 
 class URLSessionSpy: URLSession {
     var receivedURLs: [URL] = []
+    var stubbedDataTasks: [URL: URLSessionDataTask] = [:]
+
     override init() { }
+
+    func stub(url: URL, task: URLSessionDataTask) {
+        self.stubbedDataTasks[url] = task
+    }
+
     override func dataTask(with url: URL) -> URLSessionDataTask {
         self.receivedURLs.append(url)
-        return FakeURLSessionDataTask.init()
+        return self.stubbedDataTasks[url] ?? FakeURLSessionDataTask.init()
     }
 }
 
 class FakeURLSessionDataTask: URLSessionDataTask {
     override init() { }
+}
+
+class URLSessionDataTaskSpy: URLSessionDataTask {
+    var resumeCallCount: Int = 0
+
+    override init() { }
+
+    override func resume() {
+        self.resumeCallCount += 1
+    }
 }
