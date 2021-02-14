4bfa78fa3d5b45d240507049cf7c06ddc91ef503
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-02-14 18:44:26 +0100

    Test `LocalFeedLoader.save` requests cache deletion.

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -6,13 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
-    init(store: FeedStore) { }
+    let store: FeedStore
+
+    init(store: FeedStore) {
+        self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        self.store.deleteCachedFeed()
+    }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        self.deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -21,6 +34,24 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore.init()
         let _ = LocalFeedLoader.init(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore.init()
+        let items = [self.uniqueItem(), self.uniqueItem()]
+        let sut = LocalFeedLoader.init(store: store)
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+
+    // MARK: Private methods
+
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "a-desc", location: "a-loc", imageURL: self.anyURL())
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 
 }
