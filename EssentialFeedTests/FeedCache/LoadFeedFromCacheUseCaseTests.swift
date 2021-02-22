d8a4aaf8eaa9ad7b1e02054c9872b84cb6315f05
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-02-22 11:29:24 +0100

    Test `LocalFeedLoader` does not message store upon creation.

---
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoadFeedFromCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Kubilay Erdogan on 2021-02-22.
+//
+
+import XCTest
+import EssentialFeed
+
+class LoadFeedFromCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = self.makeSUT()
+        XCTAssertEqual(store.requestedCommands, [])
+    }
+
+    // MARK: Private methods
+
+    private func makeSUT(timestampProvider: @escaping () -> Date = Date.init,
+                         file: StaticString = #filePath,
+                         line: UInt = #line)
+    -> (sut: LocalFeedLoader, store: FeedStoreSpy)
+    {
+        let store = FeedStoreSpy.init()
+        let sut = LocalFeedLoader.init(store: store, timestampProvider: timestampProvider)
+        self.trackForMemoryLeak(store, file: file, line: line)
+        self.trackForMemoryLeak(sut, file: file, line: line)
+        return (sut, store)
+    }
+
+}
