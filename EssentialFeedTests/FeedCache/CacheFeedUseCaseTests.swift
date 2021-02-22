3bf167e51a5f9c3663b4613df6275232d3b1ec23
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-02-22 11:30:39 +0100

    Move `FeedStoreSpy` to a shared scope.

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -8,46 +8,6 @@
 import XCTest
 import EssentialFeed
 
-class FeedStoreSpy: FeedStore {
-
-    enum RequestedCommand: Equatable {
-        case deleteCachedFeed
-        case insertCache([LocalFeedImage], Date)
-    }
-
-    var deletionCompletions: [DeletionCompletion] = []
-    var insertionCompletions: [InsertionCompletion] = []
-    var cacheInsertions: [(feed: [LocalFeedImage], timestamp: Date)] = []
-    private(set) var requestedCommands: [RequestedCommand] = []
-
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        self.deletionCompletions.append(completion)
-        self.requestedCommands.append(.deleteCachedFeed)
-    }
-
-    func completeDeletion(with error: Error, at index: Int = 0) {
-        self.deletionCompletions[index](error)
-    }
-
-    func completeDeletionSuccessfully(at index: Int = 0) {
-        self.deletionCompletions[index](nil)
-    }
-
-    func insertCache(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        self.insertionCompletions.append(completion)
-        self.cacheInsertions.append((feed, timestamp))
-        self.requestedCommands.append(.insertCache(feed, timestamp))
-    }
-
-    func completeInsertion(with error: Error, at index: Int = 0) {
-        self.insertionCompletions[index](error)
-    }
-
-    func completeInsertionSuccessfully(at index: Int = 0) {
-        self.insertionCompletions[index](nil)
-    }
-}
-
 class CacheFeedUseCaseTests: XCTestCase {
 
     func test_init_doesNotDeleteCache_uponCreation() {
---
+++ EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,48 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Kubilay Erdogan on 2021-02-22.
+//
+
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+
+    enum RequestedCommand: Equatable {
+        case deleteCachedFeed
+        case insertCache([LocalFeedImage], Date)
+    }
+
+    var deletionCompletions: [DeletionCompletion] = []
+    var insertionCompletions: [InsertionCompletion] = []
+    var cacheInsertions: [(feed: [LocalFeedImage], timestamp: Date)] = []
+    private(set) var requestedCommands: [RequestedCommand] = []
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        self.deletionCompletions.append(completion)
+        self.requestedCommands.append(.deleteCachedFeed)
+    }
+
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        self.deletionCompletions[index](error)
+    }
+
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        self.deletionCompletions[index](nil)
+    }
+
+    func insertCache(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        self.insertionCompletions.append(completion)
+        self.cacheInsertions.append((feed, timestamp))
+        self.requestedCommands.append(.insertCache(feed, timestamp))
+    }
+
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        self.insertionCompletions[index](error)
+    }
+
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        self.insertionCompletions[index](nil)
+    }
+}
