72f5801fa25b7697eb6a02da4b10bacc3c2d9150
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-02-27 23:09:18 +0100

    Hint the clients of `FeedStore` about threading behaviour by documenting methods.

--- EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -18,7 +18,15 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
 
+    /// The completion handler can be invoked on any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+
+    /// The completion handler can be invoked on any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insertCache(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+
+    /// The completion handler can be invoked on any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
 }
