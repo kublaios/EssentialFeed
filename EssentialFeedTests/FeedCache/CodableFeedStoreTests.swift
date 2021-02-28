a1eb9c2b304b4d7515ff368c023be3b260fdbd69
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-02-28 22:01:02 +0100

    Use system domain for no-permission URL in `CodableFeedStoreTests`.

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -163,7 +163,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     }
 
     private func noDeletePermissionURL() -> URL {
-        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        return FileManager.default.urls(for: .cachesDirectory, in: .systemDomainMask).first!
     }
 
 }
