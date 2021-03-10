2e0e9e8eed77a7f506b8d7a851d79f2d70bec76b
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-03-10 14:43:20 +0100

    Create empty `CoreDataFeedStore`.

---
+++ EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -0,0 +1,22 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Kubilay Erdogan on 2021-03-10.
+//
+
+import Foundation
+
+class CoreDataFeedStore: FeedStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+
+    func insertCache(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+    }
+
+    func retrieve(completion: @escaping RetrievalCompletion) {
+
+    }
+}
