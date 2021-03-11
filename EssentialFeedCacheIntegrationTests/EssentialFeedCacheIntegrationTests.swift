6ea5563c262cbb4e3aca6b05483a0546f833ba6b
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-03-11 13:00:05 +0100

    Create `LocalFeedLoader` and `CoreDataFeedStore` integration tests target.

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Kubilay Erdogan on 2021-03-11.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
