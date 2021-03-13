a0c92ad3e16d94d93e454365a74fb962b793722b
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-03-13 16:10:04 +0100

    Add `UIRefreshControl` to prototype to simulate loading.

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -14,7 +14,30 @@ struct FeedImageViewModel {
 }
 
 class FeedViewController: UITableViewController {
-    private let prototypeFeed = FeedImageViewModel.prototypeFeed
+    private var prototypeFeed: [FeedImageViewModel] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.refreshControl?.addTarget(self, action: #selector(self.refresh), for: .valueChanged)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        self.refresh()
+    }
+
+    @objc private func refresh() {
+        self.refreshControl?.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [weak self] in
+            if self?.prototypeFeed.isEmpty == true {
+                self?.prototypeFeed = FeedImageViewModel.prototypeFeed
+                self?.tableView.reloadData()
+            }
+            self?.refreshControl?.endRefreshing()
+        }
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.prototypeFeed.count
