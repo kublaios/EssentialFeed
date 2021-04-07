6b8d2eca73a942dcbcb9b725e46b0360691a3220
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-04-07 17:27:55 +0200

    Move `FeedViewModel` and `FeedLoadingViewModel` to their own files.

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Kubilay Erdogan on 2021-04-07.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Kubilay Erdogan on 2021-04-07.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
