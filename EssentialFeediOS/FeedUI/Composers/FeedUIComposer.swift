103ed3141924154a12c45f11930e14335e0f797e
Kubilay Erdogan <kubilay@kubilayerdogan.net>
2021-04-11 16:18:41 +0200

    Move `FeedVC.makeWith` to `FeedUIComposer`.

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -13,15 +13,13 @@ public final class FeedUIComposer {
 
     public class func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter.init(feedLoader: MainQueueDispatchDecorator.init(decoratee: feedLoader))
-        let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
+        let feedController = Self.makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
         let feedViewAdapter = FeedViewAdapter.init(viewController: feedController, imageLoader: MainQueueDispatchDecorator.init(decoratee: imageLoader))
         presentationAdapter.presenter = FeedPresenter.init(feedView: feedViewAdapter, loadingView: WeekRefVirtualProxy.init(feedController))
         return feedController
     }
-}
 
-private extension FeedViewController {
-    class func makeWith(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
+    private class func makeFeedViewController(delegate: FeedViewControllerDelegate, title: String) -> FeedViewController {
         let bundle = Bundle.init(for: FeedViewController.self)
         let storyboard = UIStoryboard.init(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
